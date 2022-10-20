7e042f19d4bf67c34ce75dee5a5fa491d75d1f1b
Sonnelon <tzeentchyourea@gmail.com>
2022-10-20 11:24:02 +0300

    pops

--- Program.cs
+++ Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main(string[] args)
         {
-   
+        Console.WriteLine("Введите 1 чилсо");
+
+        double a = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите 2 чилсо");
+
+        double b = double.Parse(Console.ReadLine());
         }
     }
