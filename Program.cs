6da8838351e6416d4565dbec7931e0479428bf35
Sonnelon <tzeentchyourea@gmail.com>
2022-10-23 22:06:58 +0300

    еи

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿    
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите 1 чилсо");
+
+        double a = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите 2 чилсо");
+
+        double b = double.Parse(Console.ReadLine());
+    }
+}
