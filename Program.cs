177a7311abfac6cbdebb50d8b34c15697581f1d9
Sonnelon <tzeentchyourea@gmail.com>
2022-10-23 22:29:21 +0300

    oiil

--- Program.cs
+++ Program.cs
@@ -11,5 +11,22 @@
         Console.WriteLine("Введите 2 чилсо");
 
         double b = double.Parse(Console.ReadLine());
-        }
+
+        Der(a, b); Sub(a, b);
+
     }
+
+
+            
+            static void Der(double a, double b)
+            { 
+               Console.WriteLine(Math.Pow(a,b));
+            }
+
+            static void Sub(double a, double b)
+            {
+                Console.WriteLine(a - b);
+            }
+
+    
+}
