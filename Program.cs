348a024e9c461d1a68d2b9356bccdd4f3d3c9d32
Sonnelon <tzeentchyourea@gmail.com>
2022-10-23 23:16:00 +0300

    dwd

--- Program.cs
+++ Program.cs
@@ -4,12 +4,40 @@
     {
     static void Main(string[] args)
     {
+    static void Main(string[] args)
+    {
         Console.WriteLine("Введите 1 чилсо");
 
         double a = double.Parse(Console.ReadLine());
 
         Console.WriteLine("Введите 2 чилсо");
 
-        double b = double.Parse(Console.ReadLine());
+         Sub(a, b); Del(a, b); Sum(a, b); Der(a, b);
 
     }
+
+         
+            static void Sub(double a, double b)
+            {
+                Console.WriteLine(a - b);
+            }
+
+            static void Del(double a, double b)
+            {
+                 Console.WriteLine(a/b);
+            }
+       
+ 
+            static void Sum(double a, double b)
+            {
+                Console.WriteLine(a + b);
+            }
+
+            static void Der(double a, double b)
+            {
+                Console.WriteLine(Math.Pow(a,b));
+            }
+}
+
+}
+
